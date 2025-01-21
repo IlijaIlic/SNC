89162e8813f113abde90662137c2c616f072e79f
Ilija Ilic <ilija.ilic@elfak.rs>
2025-01-21 18:01:21 +0100

    add: model torte

--- SNCDatabase/SNCDatabase/Models/Torta.cs
+++ SNCDatabase/SNCDatabase/Models/Torta.cs
@@ -1,6 +1,19 @@
-﻿namespace SNCDatabase.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SNCDatabase.Models
 {
     public class Torta
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Naziv { get; set; }
+
+        public string? Cena { get; set; }
+
+        [ForeignKey("Poslasticar")]
+        public int PoslasticarID { get; set; }
+
     }
 }
